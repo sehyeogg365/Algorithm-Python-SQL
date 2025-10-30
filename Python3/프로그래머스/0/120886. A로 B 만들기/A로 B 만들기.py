1341566e9409024e0fbcad667d8d54eb4dd931fe
immehyeok <82208981+sehyeogg365@users.noreply.github.com>
2025-10-30 21:53:05 +0900

    [level 0] Title: A로 B 만들기, Time: 0.34 ms, Memory: 9.36 MB -BaekjoonHub

---
+++ Python3/프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.py
@@ -0,0 +1,12 @@
+def solution(before, after):
+    
+    # list = []
+    
+    sorted_before = sorted(before)
+    sorted_after = sorted(after)
+  
+    if sorted_before == sorted_after:
+        return 1
+    else :
+        return 0
+
