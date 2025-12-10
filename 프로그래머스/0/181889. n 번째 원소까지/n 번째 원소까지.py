24a22ab5c5d6c9432394a1901b4efd25e3c75065
immehyeok <82208981+sehyeogg365@users.noreply.github.com>
2025-12-10 18:24:37 +0900

    [level 0] Title: n 번째 원소까지, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/0/181889. n 번째 원소까지/n 번째 원소까지.py
@@ -0,0 +1,10 @@
+def solution(num_list, n):
+    answer = []
+    
+    for i in range(len(num_list)):
+        if i == n:
+            break       
+        answer.append(num_list[i])
+        
+        
+    return answer
