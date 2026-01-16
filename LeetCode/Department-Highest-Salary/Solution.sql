ea8b4828b874c47a96c1a63616b99b451f15de8c
immehyeok <82208981+sehyeogg365@users.noreply.github.com>
2026-01-16 22:33:49 +0900

    Add solution for Department Highest Salary

--- LeetCode/Department-Highest-Salary/Solution.sql
+++ LeetCode/Department-Highest-Salary/Solution.sql
@@ -1,13 +1,1 @@
 Department 
-WITH RankedSalary AS (
-    SELECT 
-        d.name AS Department, 
-        e.name AS Employee, 
-        e.salary,
-        DENSE_RANK() OVER (PARTITION BY d.id ORDER BY e.salary DESC) as rnk
-    FROM Employee e 
-    JOIN Department d ON e.departmentId = d.id
-)
-SELECT Department, Employee, salary
-FROM RankedSalary
-WHERE rnk = 1;
